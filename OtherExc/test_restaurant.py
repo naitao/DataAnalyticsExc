4a5b873b163b192a268e28bc46a88e063b068d86
naitao <naitao@sina.com>
2018-02-02 14:09:28 +0000

    week1 Exc

---
+++ OtherExc/test_restaurant.py
@@ -0,0 +1,12 @@
+from guest import Guest
+from restaurant import Restaurant
+g1 = Guest('Alice', 1)
+g2 = Guest('John', 2)
+g3 = Guest('Mary', 3)
+guests = [g1, g2, g3]
+r = Restaurant(2)
+while guests:
+    print(r.tables)
+    if r.seat(guests[-1]):
+        guests.pop()
+    r.serve()
